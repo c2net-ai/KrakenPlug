e5043e9b7127613e38fe3188ecd28f65f22d26d1
linfj <linfj@pcl.ac.cn>
2024-09-27 11:48:20 +0800

    add nvidia

--- build/script/copy_lib.sh
+++ build/script/copy_lib.sh
@@ -5,8 +5,8 @@ libs=("libcndev.so" "libefml.so" "libnvidia-ml.so")
 search_dirs=("/host/usr/lib/x86_64-linux-gnu" "/host/usr/local/neuware/lib64" "/host/usr/local/Ascend/driver/lib64" "/host/usr/lib" "/host/usr/lib64" )
 
 function search_and_copy {
-    local dir=$1
-    for lib in "${libs[@]}"; do
+    local lib=$1
+    for dir in "${search_dirs[@]}"; do
         for file_path in $(find "$dir" -name "$lib"); do
             echo "Found $lib in $file_path"
             cp "$file_path" "$target_dir"
@@ -16,9 +16,10 @@ function search_and_copy {
     return 1
 }
 
-for dir in "${search_dirs[@]}"; do
-    if ! search_and_copy "$dir"; then
-        break
-    fi
+for lib in "${libs[@]}"; do
+    search_and_copy "$lib"
+#    if ! search_and_copy "$lib"; then
+#        break
+#    fi
 done
 
