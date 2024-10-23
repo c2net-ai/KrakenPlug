e53fb3db5db215af04b54aa81b7255a552f22375
linfj <linfj@pcl.ac.cn>
2024-10-23 16:04:34 +0800

    change sock name

--- deviceplugin/internal/server/const.go
+++ deviceplugin/internal/server/const.go
@@ -3,5 +3,5 @@ package server
 import pluginapi "k8s.io/kubelet/pkg/apis/deviceplugin/v1beta1"
 
 const (
-	ServerSock = pluginapi.DevicePluginPath + "openi.sock"
+	ServerSock = pluginapi.DevicePluginPath + "krakenplug.sock"
 )
