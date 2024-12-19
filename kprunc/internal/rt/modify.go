2613f5090fd459d0fef6fee133b342226e9a9767
linfj <linfj@pcl.ac.cn>
2024-12-19 11:11:09 +0800

    optimize

--- kprunc/internal/rt/modify.go
+++ kprunc/internal/rt/modify.go
@@ -181,8 +181,18 @@ func (m *ModifySpec) Modify(spec *specs.Spec) error {
 	}
 
 	// 暂时先用这种方案，后续可考虑优化为hook时去ldconfig
-	spec.Process.Args = []string{"sh", "-c", fmt.Sprintf("%s;exec %s", preCmd, strings.Join(args, " "))}
+	if isShellCmd(args) {
+		spec.Process.Args = []string{args[0], args[1], fmt.Sprintf("%s;%s", preCmd, args[2])}
+	} else {
+		spec.Process.Args = []string{"sh", "-c", fmt.Sprintf("%s;exec %s", preCmd, strings.Join(args, " "))}
+	}
 
 	c.Apply(spec)
 	return nil
+}
+
+func isShellCmd(args []string) bool {
+	return len(args) == 3 &&
+		(utils.StringInSlice(args[0], []string{"bash", "sh"}) || strings.Contains(args[0], "/bin/bash") || strings.Contains(args[0], "/bin/sh")) &&
+		args[1] == "-c"
 }
